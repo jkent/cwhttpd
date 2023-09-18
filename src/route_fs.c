a42a074f46496340eef578b7383ffd28fcb95454
Jeff Kent <jeff@jkent.net>
2023-09-18 05:25:43 -0500

    use frogfs deflate

--- src/route_fs.c
+++ src/route_fs.c
@@ -34,6 +34,8 @@ Route handlers to let httpd use the filesystem to serve the files in it.
 
 #define ESPFS_FLAG_GZIP (1 << 1)
 
+#define F_REOPEN_RAW 1000
+
 
 static bool get_filepath(cwhttpd_conn_t *conn, char *path, size_t len,
         struct stat *st, const char *index)
@@ -115,23 +117,14 @@ cwhttpd_status_t cwhttpd_route_fs_get(cwhttpd_conn_t *conn)
     char buf[FILE_CHUNK_LEN];
     struct stat st;
     if (!get_filepath(conn, buf, sizeof(buf), &st, "index.html")) {
-        printf("not found\n");
         return CWHTTPD_STATUS_NOTFOUND;
     }
 
-    bool gzip_encoding = false;
+    bool deflate_compression = false;
 #if defined(ESP_PLATFORM)
-    /* Legacy ESPFS */
-    if (st.st_spare4[0] == 0x73665345) {
-        if (st.st_spare4[0] & 2) {
-            gzip_encoding = true;
-        }
-    }
-
-    /* ESPFS v2 or FrogFS*/
-    if (st.st_spare4[0] == 0x2B534645 || st.st_spare4[0] == 0x676f7246) {
-        if (st.st_spare4[0] & 1) {
-            gzip_encoding = true;
+    if (st.st_spare4[0] == 0x474F5246) { /* FROG */
+        if ((st.st_spare4[1] & 0xFF) == 1) {
+            deflate_compression = true;
         }
     }
 #endif
@@ -139,38 +132,28 @@ cwhttpd_status_t cwhttpd_route_fs_get(cwhttpd_conn_t *conn)
     const char *mimetype = cwhttpd_get_mimetype(buf);
 
     FILE *f = fopen(buf, "r");
-#if 0 // Deprecated
-    if (f == NULL) {
-        /* file not found, look for filename.gz */
-        size_t out_len = strlen(buf);
-        if (sizeof(buf) - out_len > 0) {
-            strncpy(buf + out_len, ".gz", sizeof(buf) - out_len - 1);
-            buf[sizeof(buf) - out_len - 1] = "\0";
-        }
-        f = fopen(buf, "r");
-        gzip_encoding = true;
-    }
-#endif
-
     if (f == NULL) {
         return CWHTTPD_STATUS_NOTFOUND;
     }
 
-    if (gzip_encoding) {
-        /* Check the request Accept-Encoding header for gzip. Return a 500
-         * response if not present. */
+    if (deflate_compression) {
+        /* Check the request Accept-Encoding header for deflate. Reopen raw
+         * if present */
         const char *header = cwhttpd_get_header(conn, "Accept-Encoding");
-        if (header && strstr(header, "gzip") == NULL) {
-            LOGE(__func__, "client does not accept gzip!");
-            fclose(f);
-            TRY(cwhttpd_response(conn, 500));
-            return CWHTTPD_STATUS_DONE;
+        if (header && strstr(header, "deflate") == NULL) {
+            deflate_compression = false;
         }
+    }
+    if (deflate_compression && fcntl(fileno(f), F_REOPEN_RAW) != 0) {
+        deflate_compression = false;
+    }
+    if (deflate_compression && fstat(fileno(f), &st) != 0) {
+        deflate_compression = false;
     }
 
     TRY(cwhttpd_response(conn, 200));
-    if (gzip_encoding) {
-        TRY(cwhttpd_send_header(conn, "Content-Encoding", "gzip"));
+    if (deflate_compression) {
+        TRY(cwhttpd_send_header(conn, "Content-Encoding", "deflate"));
     }
     if (mimetype) {
         TRY(cwhttpd_send_header(conn, "Content-Type", mimetype));
@@ -204,29 +187,6 @@ cwhttpd_status_t cwhttpd_route_fs_tpl(cwhttpd_conn_t *conn)
     if (!get_filepath(conn, buf, sizeof(buf), &st, "index.tpl")) {
         return CWHTTPD_STATUS_NOTFOUND;
     }
-
-#if defined(CONFIG_IDF_TARGET_ESP8266) || defined(ESP_PLATFORM)
-    bool gzip_encoding = false;
-
-    /* Legacy ESPFS */
-    if (st.st_spare4[0] == 0x73665345) {
-        if (st.st_spare4[0] & 2) {
-            gzip_encoding = true;
-        }
-    }
-
-    /* ESPFS v2 */
-    if (st.st_spare4[0] == 0x2B534645) {
-        if (st.st_spare4[0] & 1) {
-            gzip_encoding = true;
-        }
-    }
-
-    if (gzip_encoding) {
-        LOGE(__func__, "template has gzip encoding");
-        return CWHTTPD_STATUS_NOTFOUND;
-    }
-#endif
 
     const char *mimetype = cwhttpd_get_mimetype(buf);
 
